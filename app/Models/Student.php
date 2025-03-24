8b2a1ca016a4c111558c5b92ed540b9207a91fb2
carl <carllyon04@gmail.com>
2025-03-24 22:44:56 +0000

    Setup the table relationships (one-to-many) and created the required migrations

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    public function colleges(){
+        return $this->belongsTo(College::class);
+    }
 }
