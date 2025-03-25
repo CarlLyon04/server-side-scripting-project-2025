5b7cb1481fce37afa22529f456ca401dbe46fb63
carl <carllyon04@gmail.com>
2025-03-25 13:10:16 +0000

    Added CRUD operations logic, still need to test them out

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class CollegeController extends Controller
 {
-    //
+    // 1: List all colleges
+    public function index(){
+        $colleges = College::all();
+        return view('colleges.index', compact('colleges'));
+    }
+
+    // 2: Create college
+
+    public function create(){
+        return view('colleges.create');
+    }
+
+    // 3: Edit college
+
+    public function edit(College $college){
+        return view('colleges.edit', compact('college'));
+    }
 }
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,5 +7,27 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    // 1: List all students
+    public function index(){
+        $students = Student::all();
+        return view('students.index', compact('students'));
+    }
+    
+    // 2: Create student
+    
+    public function create(){
+        return view('students.create');
+    }
+    
+    // 3: Edit student
+    
+    public function edit(Student $student){
+        return view('students.edit', compact('students'));
+    }
+
+    // 4: Delete student
+    public function delete(Student $student){
+        $student->delete();
+        return redirect()->route('students.index');
+    }
 }
